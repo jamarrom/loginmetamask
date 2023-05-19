897a49dbfc79185fb6687223b5ddd5cb2b8d1cd8
Jamarrom <juanantonio.martinez2003@gmail.com>
2023-05-19 12:55:18 -0600

    Login

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,11 +4,26 @@ import { useState } from 'react';
 function Home() {
   const [error,setError] = useState(null);
   const [showAddress,setShowAddress] = useState(null);
+  const [session,setSession] = useState(false);
+  let addresAct="";
 
   const loginMetamask = () => {
     if(window.ethereum) {
-      window.ethereum.request({method: 'eth_requestAccounts'}).then(result => {
-        fetchAllData(result[0]);
+      window.ethereum.request({method: 'eth_chainId'}).then(result => {
+        if(result==="0x169")
+        {
+          window.ethereum.request({method: 'eth_requestAccounts'}).then(result => {
+            fetchAllData(result[0]);
+            addresAct = result[0];
+            setSession(true);
+          })
+        }
+        else{
+          window.ethereum=null;
+          alert("Cambia tu red a theta");
+          setError("Cambia tu red a theta");
+          setSession(false);
+        }
       })
     } else {
       setError("Por favor instala metamask");
@@ -17,12 +32,29 @@ function Home() {
 
   const fetchAllData =  (fetch) => {
     setShowAddress(fetch);
+
+    window.ethereum.on('chainChanged', (chainId) => {
+      //console.log("Cambio:" +chainId);
+
+      if(chainId==="0x169")
+      {
+        setError("");
+        setShowAddress(addresAct);
+        setSession(true);
+      }
+      else {
+        alert("Cambia tu red a theta");
+        setError("Cambia tu red a theta");
+        setShowAddress("");
+        setSession(false);
+      }
+    });
   }
 
   return(
     <div>
       METEMASK ADDRESS : {showAddress}
-      <button onClick={ loginMetamask }>Login MetaMask</button>
+      {!session?<button onClick={ loginMetamask }>Login MetaMask</button>:""}
       {error}
     </div>
   )
